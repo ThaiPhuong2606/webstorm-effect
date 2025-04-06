d895f7a58c3116dacb4f33a9d2603c1430e0c799
Robert <phuong.bt03177@sinhvien.hoasen.edu.vn>
2025-04-06 23:17:40 +0700

    feat: new demo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {CatService} from '../../services/cat.service';
 import {ProductCardComponent} from '../../components/product-card/product-card.component';
 import {CounterState} from '../../ngrx/counter/counter.state';
-import {Observable} from 'rxjs';
+import {merge, Observable} from 'rxjs';
 import {Store} from '@ngrx/store';
 
 import * as CounterActions from '../../ngrx/counter/counter.actions';
@@ -17,57 +17,84 @@ import {BookState} from '../../ngrx/book/book.state';
 import {BookModel} from '../../models/book.model';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatIcon} from '@angular/material/icon';
+import {MatInput} from '@angular/material/input';
+
+
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ProductCardComponent,
-    AsyncPipe,
+    ReactiveFormsModule,
+    MatFormField,
     MatButton,
-    MatProgressSpinner
+    MatIcon,
+    MatInput,
+    MatLabel,
+    FormsModule,
+    ReactiveFormsModule,
+    MatError
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {
 
   counter$!: Observable<number>
-  book$ : Observable<BookModel[]>
 
 
   isLoadingBook!: Observable<boolean>
 
+  loginForm = new FormGroup({})
 
-
+  emailControl = new FormControl('', [Validators.required, Validators.email])
+  errorEmail = signal('');
+  passwordControl = new FormControl('', [Validators.required, Validators.minLength(6)])
+  errorPassword = signal('');
 
   constructor(private catService: CatService,
               private store: Store<{ counter: CounterState, book: BookState }>,
               private bookService: BookService) {
-    this.counter$ = this.store.select('counter', 'count')
-    this.book$ = this.store.select('book', 'ListBooks')
-    this.isLoadingBook = this.store.select('book', 'isFetchingListBooks')
-    this.store.dispatch(BookAction.fetchListBooks());
+
+    merge(this.emailControl.statusChanges, this.emailControl.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorEmail());
+
+    merge(this.passwordControl.statusChanges, this.passwordControl.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorPassword());
+
+    this.loginForm.addControl('emailControl', this.emailControl);
+    this.loginForm.addControl('passwordControl', this.passwordControl);
+
   }
 
-  ngOnInit() {
-    this.book$.subscribe((data) => {
-      console.log(data);
-    })
+
+  printEmail() {
+  console.log(this.emailControl.value)
   }
 
-  increase() {
-    this.store.dispatch(CounterActions.increment());
+  updateErrorEmail() {
+    if (this.emailControl.hasError('required')) {
+      this.errorEmail.set('You must enter a value');
+    } else if (this.emailControl.hasError('email')) {
+      this.errorEmail.set('Not a valid email');
+    } else {
+      this.errorEmail.set('');
+    }
   }
 
-  decrease() {
-    this.store.dispatch(CounterActions.decrement());
+  updateErrorPassword() {
+    if (this.passwordControl.hasError('required')) {
+      this.errorPassword.set('You must enter a password');
+    } else if (this.passwordControl.hasError('minlength')) {
+      this.errorPassword.set('Password must be at least 6 characters');
+    } else {
+      this.errorPassword.set('');
+    }
   }
-
-  reset() {
-    this.store.dispatch(CounterActions.reset());
-  }
-
-  // get cats() {
-  //   return this.catService.cats;
-  // }
 }
